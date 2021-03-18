0136f71bf41331f0afea6ab7adbbc84711b7ef21
Kai Chang <kaijchang@gmail.com>
2021-03-18 16:27:02 -0700

    format

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app'
 
-const App: React.FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />
+const App: React.FC<AppProps> = ({ Component, pageProps }) => (
+	<Component {...pageProps} />
+)
 
 export default App
