df9e36605f386aca6b339c207e3886f412f94cdd
Kai Chang <kaijchang@gmail.com>
2021-09-01 19:25:38 -0400

    remove picture

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -11,37 +11,19 @@ const Landing: React.FC<{
 	<Page size="large">
 		<Header />
 		<Page.Content>
-			<Grid.Container gap={2}>
-				<Grid
-					xs={24}
-					md={10}
-					style={{ flexDirection: 'column', justifyContent: 'center' }}>
-					<Text h3 style={{ marginBottom: 0 }}>
-						Search ↴
-					</Text>
-					<Text b style={{ margin: 0 }}>
-						10k+ reviews & 8k+ classes
-					</Text>
-					<Spacer y={0.5} />
-					<AutosuggestSearch searchData={searchData} />
-					<style global jsx>{`
-						.react-autosuggest__container {
-							width: 100%;
-						}
-					`}</style>
-				</Grid>
-				<Grid
-					xs={24}
-					md={14}
-					style={{ flexDirection: 'column', alignItems: 'center' }}>
-					<img
-						src="/images/surr-come-back-later.svg"
-						alt="Closed"
-						style={{ height: 300 }}
-					/>
-					<Text h3>2019-2021</Text>
-				</Grid>
-			</Grid.Container>
+			<Text h3 style={{ marginBottom: 0 }}>
+				Search ↴
+			</Text>
+			<Text b style={{ margin: 0 }}>
+				10k+ reviews & 8k+ classes
+			</Text>
+			<Spacer y={0.5} />
+			<AutosuggestSearch searchData={searchData} />
+			<style global jsx>{`
+				.react-autosuggest__container {
+					width: 100%;
+				}
+			`}</style>
 		</Page.Content>
 		<Page.Footer
 			style={{
